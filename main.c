86e79f6232cafb5daa0679a2932929b44a98587b
HGHKAVEENA <as20240503@sci.sjp.ac.lk>
2025-10-23 18:48:12 +0530

    corrected previous mistake

--- main.c
+++ main.c
@@ -5,14 +5,13 @@
 #define MAX_CITIES 30
 #define MAX_DELIVERIES 50
 
-// Global arrays for cities
+
 char cities[MAX_CITIES][50];
 int cityCount = 0;
 
-// Global 2D array for distances
 int distances[MAX_CITIES][MAX_CITIES];
 
-// Global arrays for deliveries
+
 char deliverySource[MAX_DELIVERIES][50];
 char deliveryDestination[MAX_DELIVERIES][50];
 int deliveryDistance[MAX_DELIVERIES];
@@ -26,14 +25,14 @@ double customerCharge[MAX_DELIVERIES];
 double estimatedTime[MAX_DELIVERIES];
 int deliveryCount = 0;
 
-// Vehicle data - simple arrays
+
 char vehicleTypes[3][20] = {"Van", "Truck", "Lorry"};
 int vehicleCapacity[3] = {1000, 5000, 10000};
 int vehicleRate[3] = {30, 40, 80};
 int vehicleSpeed[3] = {60, 50, 45};
 int vehicleEfficiency[3] = {12, 8, 4};
 
-// Function declarations
+
 void mainMenu();
 void cityMenu();
 void distanceMenu();
@@ -52,15 +51,12 @@ void showReports();
 int main() {
     int choice;
 
-    printf("==========================================\n");
     printf("  LOGISTICS MANAGEMENT SYSTEM\n");
-    printf("==========================================\n\n");
 
     while(1) {
         mainMenu();
         printf("Enter your choice: ");
         scanf("%d", &choice);
-        getchar(); // Clear newline
 
         if(choice == 1) {
             cityMenu();
@@ -90,16 +86,16 @@ int main() {
 }
 
 void mainMenu() {
-    printf("\n==========================================\n");
+
     printf("           MAIN MENU\n");
-    printf("==========================================\n");
+
     printf("1. City Management\n");
     printf("2. Distance Management\n");
     printf("3. Vehicle Information\n");
     printf("4. Delivery Request\n");
     printf("5. Performance Reports\n");
     printf("6. Exit\n");
-    printf("==========================================\n");
+
 }
 
 void cityMenu() {
@@ -114,7 +110,7 @@ void cityMenu() {
         printf("5. Back to main menu\n");
         printf("Enter choice: ");
         scanf("%d", &choice);
-        getchar();
+
 
         if(choice == 1) {
             addCity();
